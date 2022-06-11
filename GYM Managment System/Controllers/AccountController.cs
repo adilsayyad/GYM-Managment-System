baee4d88125e595a61d27051f74feab49ee6cd5a
Adil Sayyad <adil.sayyad@waiin.com>
2022-06-11 17:49:57 +0530

    Add project files.

---
+++ GYM Managment System/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using GYM_Managment_System.Models;
+using System.Data.SqlClient;
+
+namespace GYM_Managment_System.Controllers
+{
+    
+    public class AccountController : Controller
+
+
+    {
+
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+       
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+      
+
+        void connectionString()
+        {
+            con.ConnectionString ="Data Source=WAIANGDESK22;Initial Catalog=Gymmanagement;Integrated Security=True";
+        }
+        [HttpPost]
+        public IActionResult Verify(Account acc )
+        {
+            connectionString();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "Select * from Login where UserName = '" +acc.UserName+ "' and Password = '" +acc.Password+ "'";
+            dr = com.ExecuteReader();
+            if (dr.Read())
+            {
+                con.Close();
+                return View("Create");
+            }
+            else
+            {
+                con.Close();
+                return View("Error");
+            }
+
+
+         
+        }
+    }
+}
---
+++ GYM Managment System/Models/Account.cs
@@ -0,0 +1,10 @@
+﻿namespace GYM_Managment_System.Models
+{
+    public class Account
+    {
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+    }
+}
